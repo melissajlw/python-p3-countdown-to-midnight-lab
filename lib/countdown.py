7854c170f11de660f73cd5e8e6b48236ccea7a4b
Melissa Wang <melissa.wang21@gmail.com>
2024-10-08 21:52:33 -0400

    countdown to midnight lab complete

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,13 @@
 # your code goes here!
+import time
+
+def countdown(n):
+    for i in range(n):
+        print(f"{n - i} SECOND(S)!")
+    print("HAPPY NEW YEAR!")
+
+def countdown_with_sleep(n):
+    for i in range(n):
+        print(f"{n - i} SECOND(S)!")
+        time.sleep(1)
+    print("HAPPY NEW YEAR!")
